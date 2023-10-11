06de9f6d81ba403851e9ea4c9d09587a379b0b36
Bilal Akhter <goldwinwill@gmail.com>
2023-10-11 23:39:52 +0500

    dd

--- internal/toolhelp/toolhelp.go
+++ internal/toolhelp/toolhelp.go
@@ -11,7 +11,7 @@ func ToolInfo() {
 	fmt.Println("\n Available scopes to use are:")
 	fmt.Println("\n    api     Performance testing of an API Endpoint")
 	fmt.Println("\n    cluster     Validate Kube deployment")
-	fmt.Println("\n Documentation for configuration can be found at\n\n https://www.kadriontestops.tech/docs")
+	fmt.Println("\n Documentation for configuration can be found at\n\n https://github.com/bilalakhter/kadrion")
 	fmt.Println()
 	fmt.Println(" Additional Commands --help and --version")
 	fmt.Println()
